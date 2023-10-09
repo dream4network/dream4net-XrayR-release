7698acf09e51f915c82f88082085159832fb9086
dream4network <120807232+dream4network@users.noreply.github.com>
2023-10-09 14:59:14 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -126,7 +126,7 @@ install_XrayR() {
 	else
 	    last_version="v"$1
 	fi
-        url="https://github.com/dream4network/dream4net-XrayR/releases/download/v0.9.1/XrayR-linux-64.zip"
+        url="https://github.com/dream4network/dream4net-XrayR/releases/download/${last_version}/XrayR-linux-${arch}.zip"
         echo -e "开始安装 XrayR ${last_version}"
         wget -q -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip ${url}
         if [[ $? -ne 0 ]]; then
